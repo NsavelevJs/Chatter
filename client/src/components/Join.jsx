9f11824d4aaa31ed7d7cf43e0f683b69e28687b1
NsavelevJs <nsavelevjs@gmail.com>
2020-08-27 12:49:37 -0400

    Finished styling on Join Page

--- client/src/components/Join.jsx
+++ client/src/components/Join.jsx
@@ -1,18 +1,77 @@
-import React, {userState, useState} from "react";
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Join = () => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  return(
-     <div className='font-mono'>
-   <h1 className=''>Join</h1>
-   <div><input placeholder='Name' className='' type='text' onChange={(event)=> setName(event.target.value)} /></div>
-   <div><input placeholder='Room' className='' type='text' onChange={(event)=> setRoom(event.target.value)}/></div>
-   <Link to={`/chat?name=${name}&room=${room}`}>
-   <button typeof='submit'>Sign In</button>
-   </Link>
-   </div>
-  )
+  return (
+    // 
+    //     <input
+    //       placeholder="Name"
+    //       className=""
+    //       type="text"
+    //
+    //     />
+    //   </div>
+    //   <div>
+    //     <input
+    //       placeholder="Room"
+    //       className=""
+    //       type="text"
+    //
+    //     />
+    //   </div>
+
+    <div className="w-full max-w-xs mx-auto w-1/3 p-8">
+      
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+
+        <div className="font-sans text-lg text-center ">
+       <h1 className="">Join</h1>
+      </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            for="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            for="Room"
+          >
+            Room
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={(event) => setRoom(event.target.value)}
+            type="text"
+            placeholder="Room"
+          />
+        </div>
+
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto"
+            typeof="submit"
+          >
+            Sign In
+          </button>
+        </Link>
+      </form>
+    </div>
+  );
 };
 export default Join;
