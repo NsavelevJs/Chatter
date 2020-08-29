96a0bb7e310ea4500ea9fa11c9a8b08f4a15c805
NsavelevJs <nsavelevjs@gmail.com>
2020-08-28 23:32:31 -0400

    Functionality with users is done and currently working on Chat Room page.

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -2,20 +2,28 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 let socket;
-let endpoint = 'localhost:3000'
+let endpoint = "localhost:3000";
 
 const Chat = ({ location }) => {
-  const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
-    socket = io(endpoint)
-setRoom(room)
-setName(name)
-  });
+    socket = io(endpoint);
+    setRoom(room);
+    setName(name);
 
-  return <div></div>;
+    socket.emit("join", { name, room },()=>{
+      
+    });
+
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    }
+  }, [endpoint, location.search]);
+
+  return <div className='font-sans'>Chat</div>;
 };
 export default Chat;
