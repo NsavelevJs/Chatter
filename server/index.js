a32e26e2ee08bcd33ee1e71c6f0a6f3ffddc8947
NsavelevJs <nsavelevjs@gmail.com>
2020-08-29 15:13:01 -0400

    Fixed issue with message delivery currently working on styling

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const PORT = process.env.PORT || 3000;
 const router = require("./router");
@@ -11,34 +11,42 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection',(socket)=>{
-console.log('A User is connected ')
+io.on("connect", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-socket.on('join', ({ name, room },callback)=>{
-// console.log(name,room)
-// const error = true
-const {error, user}= addUser({id:socket.id,name,room})
+    if (error) return callback(error);
 
-if(error) return callback(error);
+    socket.join(user.room);
 
-socket.emit('message',{user:'admin',text:`${user.name},Welcome to room${user.room}`})
-socket.broadcast.to(user.room).emit('message',{user:'admin',text:`${user.name}, has joined.`})
-socket.join(user.room)
-callback()
-})
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to room ${user.room}.`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
 
-socket.on('sendMessage', (message,callback)=>{
-const user = getUser(socket.id)
-io.to(user.room).emit('message',{user: user.name,text:message})
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
-callback()
-})
-
-socket.on('disconnect', () => {
-    console.log('user disconnected');
-    
+    callback();
   });
-})
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 app.use(router);
 server.listen(PORT, () => console.log(`Server is up and running on ${PORT}`));
